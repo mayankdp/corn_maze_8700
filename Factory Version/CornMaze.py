fcae6765dfebbccfab1b095b9220f0bb8ac194f1
MattiaGalanti <70184042+MattiaGalanti@users.noreply.github.com>
2022-11-29 13:33:08 -0500

    Add files via upload
    Factory implemented

---
+++ Factory Version/CornMaze.py
@@ -0,0 +1,126 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 15 12:39:17 2022
+
+@author: sushmaamara, Mayank, Parita, Mattie
+"""
+
+import os
+from tkinter import messagebox
+import pygame
+from pygame import mixer
+from tkinter import *
+from tkinter import messagebox
+import sys
+from singleton import Singleton 
+from Player import *
+
+
+global welcomescreen
+welcomescreen = False
+
+pygame.init()
+background = pygame.image.load('/Users/mattiagalanti/Desktop/CornMazeNew/Images/background.png')
+pygame.display.set_caption("Corn Maze")
+icon = pygame.image.load('/Users/mattiagalanti/Desktop/CornMazeNew/Images/icongame.ico')
+pygame.display.set_icon(icon)
+main_font = pygame.font.SysFont("cambria", 50)
+screen.blit(background,(0,0))
+
+class Button():
+	def __init__(self, image, x_pos, y_pos, text_input):
+		self.image = image
+		self.x_pos = x_pos
+		self.y_pos = y_pos
+		self.rect = self.image.get_rect(center=(self.x_pos, self.y_pos))
+		self.text_input = text_input
+		self.text = main_font.render(self.text_input, True, (255, 0, 0))
+		self.text_rect = self.text.get_rect(center=(self.x_pos, self.y_pos))
+
+	def update(self):
+		screen.blit(self.image, self.rect)
+		screen.blit(self.text, self.text_rect)
+
+	def checkForInput(self, position):
+		if position[0] in range(self.rect.left, self.rect.right) and position[1] in range(self.rect.top, self.rect.bottom) and welcomescreen == True :
+			print("Button Press!")
+			return True		
+
+	def changeColor(self, position):
+		if position[0] in range(self.rect.left, self.rect.right) and position[1] in range(self.rect.top, self.rect.bottom):
+			self.text = main_font.render(self.text_input, True, (0, 0, 255))
+		else:
+			self.text = main_font.render(self.text_input, True, (255, 0, 0))
+
+class GameState():
+    def __init__(self):
+        self.state = 'main_game'
+
+    def main_game(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                output=button.checkForInput(pygame.mouse.get_pos())
+                if output == True:
+                    welcomescreen = False
+                    newscreen = pygame.display.set_mode((800, 500))
+                    screen.fill((73,56,112))
+                    for wall in walls:
+                        screen.blit(pumpkin,wall.rect)
+                        #pygame.draw.rect(screen, (255, 200, 125), player.rect)
+                        pygame.draw.rect(screen, (73,56,112), singleton.player.rect)
+                        playerImg = pygame.image.load("/Users/mattiagalanti/Desktop/CornMazeNew/Images/marathon.png")
+                        playerImg = pygame.transform.scale(playerImg, (38, 38))
+                        screen.blit(playerImg, pygame.Rect(32, 32, 20, 20))
+                        pygame.draw.rect(screen, (255, 0, 0), end_rect)
+                        mixer.init()
+                        mixer.music.load(r"/Users/mattiagalanti/Desktop/CornMazeNew/Music/background.wav")
+                        mixer.music.play(loops=-1)
+                        
+            
+            if event.type == pygame.KEYDOWN:
+                # print("Hi")
+                if event.key == pygame.K_LEFT:
+                    # print("key Left")
+                    singleton.player.move(-10, 0)
+                if event.key == pygame.K_RIGHT:
+                    singleton.player.move(10, 0)
+                if event.key == pygame.K_UP:
+                    singleton.player.move(0, -10)
+                if event.key == pygame.K_DOWN:
+                    singleton.player.move(0, 10)
+    
+            if singleton.player.rect.colliderect(end_rect):
+                Tk().wm_withdraw() #to hide the main window
+                messagebox.showinfo('Continue','OK')
+                pygame.quit()
+                sys.exit()
+
+        pygame.display.flip()
+        clock.tick(360)
+
+os.environ["SDL_VIDEO_CENTERED"] = "1"
+ 
+clock = pygame.time.Clock()
+
+button_surface = pygame.image.load("/Users/mattiagalanti/Desktop/CornMazeNew/Images/button.png")
+button_surface = pygame.transform.scale(button_surface, (300, 100))
+button = Button(button_surface, 400, 250, "START")
+button.update()
+button.changeColor(pygame.mouse.get_pos())
+welcomescreen = True
+
+# Singleton 
+singleton = Singleton.getInstance()
+singleton.player = Player()
+
+
+click = False
+running = True
+game = GameState()
+while running:
+    game.main_game()
---
+++ Factory Version/Player.py
@@ -0,0 +1,68 @@
+import pygame
+from wall import *
+import random
+from Pattern import *
+
+
+screen = pygame.display.set_mode((798, 500))
+pumpkin=pygame.image.load("/Users/mattiagalanti/Desktop/CornMazeNew/Images/pumpkin.png")
+pumpkin=pygame.transform.scale(pumpkin,(30,30))
+
+
+#generate random level
+
+pattern = random.randint(1,3)
+
+maze_pattern = CreateMazeWall.rise(pattern)
+
+# Parse the level string above. W = wall, E = exit
+x = y = 1
+for row in maze_pattern:
+    for col in row:
+        if col == "W":
+            Wall((x, y))
+        if col == "E":
+            end_rect = pygame.Rect(x, y, 20, 20)
+        x += 30
+    y += 29
+    x = 1
+
+class Player(object): 
+    def __init__(self):
+        self.rect = pygame.Rect(30, 30, 20, 20)
+ 
+    def move(self, dx, dy):
+        if dx != 0:
+            # print("inside move")
+            # print(dx)
+            self.move_single_axis(dx, 0)
+        if dy != 0:
+            self.move_single_axis(0, dy)
+            
+        screen.fill((73,56,112))
+        for wall in walls:
+                screen.blit(pumpkin,wall.rect)
+                pygame.draw.rect(screen, (73,56,112), Player().rect)
+                playerImg = pygame.image.load("/Users/mattiagalanti/Desktop/CornMazeNew/Images/marathon.png")
+                playerImg = pygame.transform.scale(playerImg, (38, 38))
+                screen.blit(playerImg, self.rect)
+                
+                pygame.draw.rect(screen, (255, 0, 0), end_rect)
+ 
+    def move_single_axis(self, dx, dy):
+        # print("inside move single axis")
+        self.rect.x += dx
+        self.rect.y += dy
+        self.collision(dx, dy)
+ 
+    def collision(self, dx, dy):
+        for wall in walls:
+            if self.rect.colliderect(wall.rect):
+                if dx > 0:
+                    self.rect.right = wall.rect.left
+                if dx < 0:
+                    self.rect.left = wall.rect.right
+                if dy > 0:
+                    self.rect.bottom = wall.rect.top
+                if dy < 0:
+                    self.rect.top = wall.rect.bottom
